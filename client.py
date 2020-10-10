8396d3f42ce7e7ffb68088b5810c438eafdc85cf
Fausto Márquez <fmarquez@globaluy.com>
2020-10-10 15:42:17 -0300

    :construction: Work in progress, connections being stablished but with errors

--- client.py
+++ client.py
@@ -1,25 +1,20 @@
 #!./redes2/bin/python
 
 import socket
+import selectors
+import types
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '127.0.0.1'  # Server IP
+PORT = 2020        # Listening socket en el server
+CONNECTIONS = 3     # Cuántas conexiones establece
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
-
-messages = [b'Message 1 from client.', b'Message 2 from client.']
-
+sel = selectors.DefaultSelector()
 
 def start_connections(host, port, num_conns):
     server_addr = (host, port)
     for i in range(0, num_conns):
         connid = i + 1
-        print('starting connection', connid, 'to', server_addr)
+        print('Estableciendo conexión', connid, 'a', server_addr)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(False)
         sock.connect_ex(server_addr)
@@ -30,3 +25,35 @@ def start_connections(host, port, num_conns):
                                      messages=list(messages),
                                      outb=b'')
         sel.register(sock, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Debe estar ready to read
+        if recv_data:
+            print('Recibido', repr(recv_data), 'de la conexión', data.connid)
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print('Cerrando conexión', data.connid)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print('Enviando', repr(data.outb), 'a la conexión', data.connid)
+            sent = sock.send(data.outb)  # Debe estar ready to write
+            data.outb = data.outb[sent:]
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hola mundo')
+    data = s.recv(1024)
+
+print('Recibido', repr(data))
+
+messages = [b'Mensaje 1 de cliente.', b'Mensaje 2 de cliente.']
+
+start_connections(HOST, PORT, CONNECTIONS)
--- server.py
+++ server.py
@@ -2,12 +2,39 @@
 
 import selectors
 import socket
+import types
 
 HOST = '127.0.0.1' 
-PORT = 65432
+PORT = 2020
+
+def accept_wrapper(lis_socket):
+    conn, addr = lis_socket.accept() # conn es la nueva conexión (socket) para este nuevo cliente
+    print('Conexión aceptada desde', addr)
+    conn.setblocking(False) # El listening socket (lis_socket) debe seguir ready to read, por eso ponemos este nuevo en non-blocking. Así no tranca el (los) otro(s)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    socket = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = socket.recv(1024)  # Debe estar ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('Cerrando conexión a', data.addr)
+            sel.unregister(socket)
+            socket.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('Enviando', repr(data.outb), 'a', data.addr)
+            sent = socket.send(data.outb)  # Debe estar ready to write
+            data.outb = data.outb[sent:]
+
 
 sel = selectors.DefaultSelector()
-# ...
+
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((HOST, PORT))
 lsock.listen()
@@ -21,33 +48,7 @@ while True:
     for key, mask in events:
         if key.data is None:
             # Sabemos que es el listening socket, entonces aceptamos la conexión registrando un nuevo socket en el selector
-            accept_wrapper(key.fileobj) # key.fileobj es nuestro socket
+            accept_wrapper(key.fileobj) # key.fileobj es nuestro listening socket
         else:
             # Como hay data sabemos que es de un socket cliente ya aceptado, entonces servimos
             service_connection(key, mask)
-
-
-def accept_wrapper(lis_socket):
-    conn, addr = lis_socket.accept() # conn es la nueva conexión (socket) para este nuevo cliente
-    print('Conexión aceptada desde ', addr)
-    conn.setblocking(False) # El listening socket (lis_socket) debe seguir ready to read, por eso ponemos este nuevo en non-blocking. Así no tranca el (los) otro(s)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print('Cerrando conexión a ', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
