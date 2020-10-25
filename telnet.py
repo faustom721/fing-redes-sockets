929e2afe83a418e820dd8ca69891c61c67688596
Crhistyan Silva <cr.silper@gmail.com>
2020-10-25 19:52:54 -0300

    arreglo en download

--- telnet.py
+++ telnet.py
@@ -53,15 +53,14 @@ def process_download(download):
         with open(file_path, "rb") as f:
             f.read(int(start))
             chunk = f.read(int(size))
-        return 'DOWNLOAD OK\n' + chunk
+        return b'DOWNLOAD OK\n' + chunk
 
 
 download_manager = (None, {}) # (file_name, {socket: (id_chunk, chunk, recieved)})
 
 def process_file_chunk(sock, chunk):
     global download_manager
-    chunk = chunk.splitlines()[1]
-    download_manager[1][sock][1] = chunk
+    download_manager[1][sock][1] = chunk[12:]
     download_manager[1][sock][2] = True
 
     ready = 0
